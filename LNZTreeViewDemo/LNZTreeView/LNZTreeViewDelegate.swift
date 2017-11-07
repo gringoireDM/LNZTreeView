af6a3ac1d6912729239cd1cbfc526ee36bf8a27a
Giuseppe Lanza <gringoire986@gmail.com>
2017-11-07 17:35:59 +0000

    Initial Commit

---
+++ LNZTreeViewDemo/LNZTreeView/LNZTreeViewDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  LNZTreeViewDelegate.swift
+//  PFRPG rd
+//
+//  Created by Giuseppe Lanza on 24/09/2017.
+//  Copyright © 2017 Giuseppe Lanza. All rights reserved.
+//
+
+import Foundation
+
+@objc public protocol LNZTreeViewDelegate {
+    /**
+     This method is called when a node is successfully expanded. The indexPath is relative to the
+     *parentNode* parameter.
+     
+     ## Example:
+     An *indexPath* with row **i** and section **j** in parentNode **A** means the **i**th child of parentNode in the
+     section **j**. If not present parentNode, the requested node is the ith element in the root of the section **j**.
+
+     - parameter treeView: The tree view on which the event was triggered.
+     - parameter indexPath: The indexPath of the expanded node, relative to its *parentNode*.
+     - parameter parentNode: The parentNode for the expanded node. If nil, root is to be intended.
+     */
+    func treeView(_ treeView: LNZTreeView, didExpandNodeAt indexPath: IndexPath, forParentNode parentNode: TreeNode?)
+    
+    /**
+     This method is called when a node is successfully collapsed. The indexPath is relative to the
+     *parentNode* parameter.
+     
+     ## Example:
+     An *indexPath* with row **i** and section **j** in parentNode **A** means the **i**th child of parentNode in the
+     section **j**. If not present parentNode, the requested node is the ith element in the root of the section **j**.
+     
+     - parameter treeView: The tree view on which the event was triggered.
+     - parameter indexPath: The indexPath of the collapsed node, relative to its *parentNode*.
+     - parameter parentNode: The parentNode for the collapsed node. If nil, root is to be intended.
+     */
+    func treeView(_ treeView: LNZTreeView, didCollapseNodeAt indexPath: IndexPath, forParentNode parentNode: TreeNode?)
+    
+    /**
+     This method is called when a node is successfully selected. The indexPath is relative to the
+     *parentNode* parameter.
+     
+     ## Example:
+     An *indexPath* with row **i** and section **j** in parentNode **A** means the **i**th child of parentNode in the
+     section **j**. If not present parentNode, the requested node is the ith element in the root of the section **j**.
+     
+     - parameter treeView: The tree view on which the event was triggered.
+     - parameter indexPath: The indexPath of the selected node, relative to its *parentNode*.
+     - parameter parentNode: The parentNode for the selected node. If nil, root is to be intended.
+     */
+    func treeView(_ treeView: LNZTreeView, didSelectNodeAt indexPath: IndexPath, forParentNode parentNode: TreeNode?)
+}
