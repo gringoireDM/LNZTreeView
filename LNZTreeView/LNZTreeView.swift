0f50169e284d23e16cbe0d81c50f354cfe2eaaf2
Vitaliy Samoylik <vitaliy.samoylik@solarwinds.com>
2019-10-16 15:50:35 +0300

    Suppress compiler warning

--- LNZTreeView/LNZTreeView.swift
+++ LNZTreeView/LNZTreeView.swift
@@ -485,7 +485,7 @@ extension LNZTreeView: UITableViewDelegate {
     }
     
     public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        guard var nodes = nodesForSection[indexPath.section],
+        guard let nodes = nodesForSection[indexPath.section],
             let indexInParent = self.indexInParent(forNodeAt: indexPath) else {
                 fatalError("Something wrong here")
         }
